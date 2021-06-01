80d7594b156e4350bf06b22afed9ac5b959cf170
hag007 <hagai.levi.007@gmail.com>
2021-06-01 10:02:51 +0300

    clean code

--- src/implementations/network_builder.py
+++ src/implementations/network_builder.py
@@ -4,10 +4,9 @@ import pandas as pd
 
 def build_network(network_file):
     """"""
-    edges_dataset = pd.read_csv(network_file, sep='\t', header=None)
+    edges_dataset = pd.read_csv(network_file, sep='\t', header=None, dtype=str)
     edges = []
     for ind, row in edges_dataset.iterrows():
-        # if row.iloc[0]!=row.iloc[2]:
         edges.append((row.iloc[0], row.iloc[2]))
     G = nx.Graph()
     G.add_edges_from(edges)
--- src/runners/run_algo.py
+++ src/runners/run_algo.py
@@ -3,17 +3,8 @@ sys.path.insert(0, '../')
 import os
 from src import constants
 from src.runners.domino_runner import DominoRunner
-from src.runners.netbox_runner import NetboxRunner
-from src.runners.jactivemodules_greedy_runner import jAMGreedyRunner
-from src.runners.jactivemodules_sa_runner import jAMSARunner
-from src.runners.bionet_runner import BionetRunner
-from src.runners.keypathwayminer_ines_greedy_runner import KPMRunner
-from src.runners.hotnet2_runner import Hotnet2Runner
 
-ALGO_BY_NAMES = {"netbox": NetboxRunner(), "jactivemodules_greedy": jAMGreedyRunner(),
-                 "jactivemodules_greedy_string": jAMGreedyRunner(), "jactivemodules_sa": jAMSARunner(),
-                 "bionet": BionetRunner(), "bionet_string": BionetRunner(), 'keypathwayminer_INES_GREEDY': KPMRunner(),
-                 'hotnet2': Hotnet2Runner(), 'DOMINO': DominoRunner()}
+ALGO_BY_NAMES = {'DOMINO': DominoRunner()}
 
 
 def add_algo_runner(k,v):
